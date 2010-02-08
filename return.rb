202f29c9bf779dfc4dfd59a535ef0360fc1f886f
Justin Love <hg@JustinLove.name>
2010-02-07 22:33:02 -0600

    remove spurious returns

--- return.rb
+++ return.rb
@@ -1,19 +1,19 @@
 def thrice
-  return [ yield(1), yield(2), yield(3) ]
+  [ yield(1), yield(2), yield(3) ]
 end
 
 def r
-  return thrice {|a| return a}.to_s
+  thrice {|a| return a}.to_s
 end; r
 
 def b
-  return thrice {|a| break  a}.to_s
+  thrice {|a| break  a}.to_s
 end; b
 
 def n
-  return thrice {|a| next   a}.to_s
+  thrice {|a| next   a}.to_s
 end; n
 
 def x
-  return thrice {|a|        a}.to_s
+  thrice {|a|        a}.to_s
 end; x

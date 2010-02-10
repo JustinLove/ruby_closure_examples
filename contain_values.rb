088b76d2ac1f492be0cbe3038ee728194422fbdf
Justin Love <hg@JustinLove.name>
2010-02-09 19:39:13 -0600

    rename context to contain_values
    --HG--
    rename : context.rb => contain_values.rb

--- contain_values.rb
+++ contain_values.rb
@@ -1,9 +1,9 @@
-f = File.open('context.rb')
+f = File.open('contain_values.rb')
 f.read.length
 # Opps, forgot to close it
 f.closed?
 
-File.open('context.rb') do |f|
+File.open('contain_values.rb') do |f|
   $f = f
   f.read.length
 end

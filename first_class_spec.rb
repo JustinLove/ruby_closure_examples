769ccae1ceb8f603ae1a8524162fb31342ae757f
Justin Love <hg@JustinLove.name>
2010-04-04 18:50:03 -0500

    reformat first class specs

--- first_class_spec.rb
+++ first_class_spec.rb
@@ -1,17 +1,28 @@
-require 'rubygems'; require 'spec'
+require 'rubygems'
+require 'spec'
+
 describe "First Class Functions" do
   it "may be named by variables" do
-    x = lambda {:foo}
-    x.call.should == :foo
+    x = lambda {:soul}
+    x.call.should == :soul
   end
+
   it "may be stored in data structures" do
-    x = [lambda {:foo}]
-    x.first.call.should == :foo
+    x = [lambda {:soul}]
+    x.first.call.should == :soul
   end
+
   it "may be passed as an argument" do
-    lambda{|x| x.call.should == :foo}.call(lambda {:foo})
+    def swallow(x)
+      x.call.should == :soul
+    end
+    swallow(lambda {:soul})
   end
+
   it "may be returned as a result" do
-    lambda{ lambda {:foo} }.call.call.should == :foo
+    def well
+      lambda {:soul}
+    end
+    well.call.should == :soul
   end
 end
